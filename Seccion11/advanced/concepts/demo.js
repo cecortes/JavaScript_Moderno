02e4df56072c52960d5a9d6f7f9c35c38c4c010c
Cesar Lopez <cesarlopezcortes@me.com>
2026-01-24 09:09:47 -0600

    Sección 11 - Variables de entorno en Vite

---
+++ Seccion11/advanced/concepts/demo.js
@@ -0,0 +1,6 @@
+/**
+ * environmentsComponent
+ * @description Recibe un div y le inyecta un contenido
+ * @param {HTMLDivElement} element
+ */
+export const environmentsComponent = (element) => {};
