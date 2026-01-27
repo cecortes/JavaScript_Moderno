ea4e0c810a3e5318cf9fb6cd75e0555ae8afdf17
Cesar Lopez <cesarlopezcortes@me.com>
2026-01-26 20:05:51 -0600

    Sección 11 - Promises - Promise Hell - Promise All - Promise Race

---
+++ Seccion11/advanced/concepts/03-promises.js
@@ -0,0 +1,74 @@
+import { heroes } from "../data/heroes";
+
+/**
+ * environmentsComponent
+ * @description Recibe un div y le inyecta un contenido
+ * @param {HTMLDivElement} element
+ */
+export const promiseComponent = (element) => {
+  const renderHero = (hero) => {
+    element.innerHTML = hero.name;
+  };
+
+  const renderTwoHero = (hero1, hero2) => {
+    element.innerHTML = `
+      <h3> ${hero1.name} </h3>
+      <h3> ${hero2.name} </h3>
+    `;
+  };
+
+  const renderError = (error) => {
+    element.innerHTML = `<h3> ${error} </h3>`;
+  };
+
+  const id1 = "5d86371f25a058e5b1c8a65e";
+  const id2 = "5d86371f9f80b591f499df32";
+  let hero1;
+
+  // Forma natural
+  //findHero(id1).then((superHeroe) => renderHero(superHeroe));
+
+  // Forma función como referencia
+  //findHero(id1).then(renderHero).catch(renderError);
+
+  //! Sin promise hell
+  // findHero(id1)
+  //   .then((hero) => {
+  //     hero1 = hero;
+  //     return findHero(id2);
+  //   })
+  //   .then((hero2) => {
+  //     renderTwoHero(hero1, hero2);
+  //   })
+  //   .catch(renderError);
+
+  // Promise.all
+  Promise.all([findHero(id1), findHero(id2)])
+    .then(([hero1, hero2]) => {
+      renderTwoHero(hero1, hero2);
+    })
+    .catch(renderError);
+};
+
+/**
+ * @function findHero
+ * @description Busca un heroe por el id que recibe
+ * @param {String} id
+ * @returns {Promise}
+ */
+const findHero = (id) => {
+  return new Promise((resolve, reject) => {
+    const hero = heroes.find((hero) => hero.id === id);
+
+    // Bloque decisión para resolve
+    if (hero) {
+      resolve(hero);
+      return;
+    }
+
+    // Bloque reject
+    reject(`Hero with id: ${id}, not found`);
+  });
+
+  // Return Promise
+};
--- Seccion11/advanced/src/main.js
+++ Seccion11/advanced/src/main.js
@@ -3,6 +3,8 @@ import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { environmentsComponent } from "../concepts/01-environments";
 import { callbacksComponent } from "../concepts/02-callbacks";
+import { promiseComponent } from "../concepts/03-promises";
+import { promiseRaceComponent } from "../concepts/04-promise-race";
 
 document.querySelector("#app").innerHTML = `
   <div>
@@ -23,4 +25,4 @@ document.querySelector("#app").innerHTML = `
 const element = document.querySelector(".card");
 
 //environmentsComponent(element);
-callbacksComponent(element);
+promiseRaceComponent(element);
